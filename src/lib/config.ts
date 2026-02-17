1652b2a1a21bd6e9a54b3f441d28077887e08360
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2026-02-17 23:29:53 +0100

    wip

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,7 +2,6 @@
  * Application configuration constants
  */
 
-import { PUBLIC_BASE_MODEL_URL } from '$env/static/public';
-
 export const BASE_MODEL_URL =
-	PUBLIC_BASE_MODEL_URL || 'https://sta-public.fra1.cdn.digitaloceanspaces.com';
+	import.meta.env.PUBLIC_BASE_MODEL_URL || // import.meta to work with Web Workers
+	'https://sta-public.fra1.cdn.digitaloceanspaces.com';
